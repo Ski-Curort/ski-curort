e014ec441c0233c3b9172e491c1f0fa1bb7927df
Tomasz Poniatowski <tomasz.poniatowski@cytawa.com>
2022-10-25 18:57:16 +0200

    Initial commit

---
+++ src/main/java/com/example/skicurort/SkiCurortApplication.java
@@ -0,0 +1,13 @@
+package com.example.skicurort;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkiCurortApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SkiCurortApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/skicurort/SkiCurortApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.skicurort;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkiCurortApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
