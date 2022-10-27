a0ec235a1463f66e181692964f7888da59846ce3
Marcin <szpak111@gmail.com>
2022-10-27 19:25:54 +0200

    -Create entity to Equipment-

--- src/main/java/com/example/skicurort/equipment/Equipment.java
+++ src/main/java/com/example/skicurort/equipment/Equipment.java
@@ -1,4 +1,28 @@
 package com.example.skicurort.equipment;
 
+import com.example.skicurort.curort.Curort;
+import com.example.skicurort.shop.Shop;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+
+import javax.persistence.*;
+
+@Entity
+@Setter
+@Getter
+@NoArgsConstructor
+@ToString
 public class Equipment {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String type;
+    private String mark;
+    private boolean available;
+    private float cost;
+    @ManyToOne
+    private Shop shop;
 }
