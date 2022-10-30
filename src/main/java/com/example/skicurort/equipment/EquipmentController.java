cdb4dff00fbe14525758f215644d520c4dbb2cc8
Marcin <szpak111@gmail.com>
2022-10-30 13:22:34 +0100

    -add methods in equipmentController-

--- src/main/java/com/example/skicurort/equipment/EquipmentController.java
+++ src/main/java/com/example/skicurort/equipment/EquipmentController.java
@@ -1,21 +1,40 @@
 package com.example.skicurort.equipment;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
+@RequestMapping("/api")
 public class EquipmentController {
      private final EquipmentService equipmentService;
     @Autowired
-    public EquipmentController(EquipmentService equipmentService) {
+    EquipmentController(EquipmentService equipmentService) {
         this.equipmentService = equipmentService;
     }
-    @GetMapping
+    @GetMapping("/all")
     List<Equipment> getAllEquipment(){
        return equipmentService.getAll();
     }
+    @GetMapping("/type/{type}")
+    List<Equipment> getEquipmentByType(String type){
+        return equipmentService.getAllEquipmentByType(type);
+    }
+    @PostMapping("/newEquipment")
+    Equipment addNewEqupiment(Equipment equipment){
+        return equipmentService.addEquipment(equipment);
+    }
+    @PutMapping("/update/{id}")
+    Equipment updateEquipment(Long id,Equipment equipment){
+        return equipmentService.editEquipment(id,equipment);
+    }
+    @DeleteMapping("/delete/{id}")
+        Equipment removeEquipment(Long id){
+            return equipmentService.deleteEquipment(id);
+        }
+    }
+
+
 
 }
