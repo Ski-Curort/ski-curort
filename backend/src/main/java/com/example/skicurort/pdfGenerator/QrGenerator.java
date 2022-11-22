790e746165eb7b8c25f98d2e7b0b661495f34b53
Tomasz Poniatowski <tomasz.poniatowski@cytawa.com>
2022-11-22 23:02:57 +0100

    -add QrCode Generator-

---
+++ backend/src/main/java/com/example/skicurort/pdfGenerator/QrGenerator.java
@@ -0,0 +1,37 @@
+package com.example.skicurort.pdfGenerator;
+
+import com.google.zxing.BarcodeFormat;
+import com.google.zxing.MultiFormatWriter;
+import com.google.zxing.WriterException;
+import com.google.zxing.client.j2se.MatrixToImageWriter;
+import com.google.zxing.common.BitMatrix;
+import java.io.File;
+import java.io.IOException;
+import java.util.Calendar;
+
+public class QrGenerator {
+  public static void generateQRcode(String data, String path, String charset, int h, int w)
+      throws WriterException, IOException {
+    BitMatrix matrix =
+        new MultiFormatWriter()
+            .encode(new String(data.getBytes(charset), charset), BarcodeFormat.QR_CODE, w, h);
+    MatrixToImageWriter.writeToFile(
+        matrix, path.substring(path.lastIndexOf('.') + 1), new File(path));
+  }
+
+  public String generate(Long id) throws IOException, WriterException {
+    String content = String.valueOf(id);
+    Calendar calendar = Calendar.getInstance();
+    String path =
+        "Quote_"
+            + calendar.get(Calendar.HOUR_OF_DAY)
+            + "_"
+            + calendar.get(Calendar.MINUTE)
+            + "_"
+            + calendar.get(Calendar.SECOND)
+            + ".png";
+    String charset = "UTF-8";
+    generateQRcode(content, path, charset, 200, 200);
+    return path;
+  }
+}
