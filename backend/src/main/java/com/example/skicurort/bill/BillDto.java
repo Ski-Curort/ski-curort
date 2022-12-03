610948a7b45282a9894c135bd626f5e30fc64e63
Tomasz Poniatowski <tomasz.poniatowski@cytawa.com>
2022-12-03 18:35:49 +0100

    -fix-

--- backend/src/main/java/com/example/skicurort/bill/BillDto.java
+++ backend/src/main/java/com/example/skicurort/bill/BillDto.java
@@ -3,13 +3,12 @@ package com.example.skicurort.bill;
 import com.example.skicurort.curort.Curort;
 import com.example.skicurort.item.Item;
 import java.math.BigDecimal;
-import java.util.Date;
 import java.util.List;
 
 public record BillDto(
     Long id,
     BigDecimal totalCost,
-    Date creationDate,
+    String creationDate,
     String userName,
     Curort curort,
     List<Item> itemList) {}
--- frontend/src/components/Cart.tsx
+++ frontend/src/components/Cart.tsx
@@ -11,10 +11,12 @@ import UserContext from "../context/UserContext";
 import {AxiosResponse} from "axios";
 import {ResortData} from "../models/resorts";
 import {BillData} from "../models/bill";
+import {Item} from "../models/item";
 
-export const Cart  = () => {
+export const Cart = () => {
 
     const equipments = [{
+
         itemId: 1,
         equipmentType: "Ski",
         brand: "gf",
@@ -43,81 +45,77 @@ export const Cart  = () => {
     const navigate = useNavigate()
     const context = useContext(DataContext);
     const [bill, setBill] = useState(context.billData)
-    const [resort, setResort] = useState(context.billData.curort)
-    const contextUser=useContext(UserContext)
+    const contextUser = useContext(UserContext)
 
 
     useEffect(() => {
         authorizedApi.post(`${process.env.REACT_APP_API_BASE_URL}/api/bill/${contextUser.currentUser?.displayName}`,
-        ).then((res:AxiosResponse<BillData>)=>{
+        ).then((res: AxiosResponse<BillData>) => {
             setBill(res.data)
 
         })
-    },[]);
-
-
-context.billDataModifier(bill)
-
+    }, []);
 
     function confirmBill() {
         equipments.forEach((equipment) => {
 
-             authorizedApi.post(`${process.env.REACT_APP_API_BASE_URL}/api/item/${bill.id}`,
+            authorizedApi.post(`${process.env.REACT_APP_API_BASE_URL}/api/item/${bill.id}`,
                 {
-                "id": equipment.itemId,
+                    "id": equipment.itemId,
                     "type": equipment.equipmentType,
                     "mark": equipment.brand,
                     "cost": equipment.totalPrice
-            });
+                }).then(()=>navigate("/confirmation"));
 
-        })
-    }
+        })}
 
-    function deleteItem(id: number) {
-        const newEquipments = equipments.filter(equipment => equipment.itemId !== id)
-        equipments.splice(0, equipments.length);
-        return equipments.concat(newEquipments);
-    }
+    context.billDataModifier(bill)
 
-    return (
-        <Box>
-            <Box display={"flex"} justifyContent={"center"} flexDirection={"column"} alignItems={"center"}>
-                <p className={"summary"}>Summary:</p>
+        function deleteItem(id: number) {
+            const newEquipments = equipments.filter(equipment => equipment.itemId !== id)
+            equipments.splice(0, equipments.length);
+            return equipments.concat(newEquipments);
+        }
 
-                <Box width='908px'>
-                    <Box className={"summaryBar"} background={"white"} height='40px' display={"flex"}
-                         flexDirection={"row"} marginTop='12px' marginBottom='12px'>
-                        <Box width='210px' marginLeft='16px' paddingLeft='24px'>Equipment Type</Box>
-                        <Box width='252px' paddingLeft='24px'>Brand</Box>
-                        <Box width='130px' paddingLeft='24px'>Cost</Box>
-                        <Box width='175px' paddingLeft='24px'>Amount</Box>
-                        <Box width='96px'></Box>
+        return (
+            <Box>
+                <Box display={"flex"} justifyContent={"center"} flexDirection={"column"} alignItems={"center"}>
+                    <p className={"summary"}>Summary:</p>
+
+                    <Box width='908px'>
+                        <Box className={"summaryBar"} background={"white"} height='40px' display={"flex"}
+                             flexDirection={"row"} marginTop='12px' marginBottom='12px'>
+                            <Box width='210px' marginLeft='16px' paddingLeft='24px'>Equipment Type</Box>
+                            <Box width='252px' paddingLeft='24px'>Brand</Box>
+                            <Box width='130px' paddingLeft='24px'>Cost</Box>
+                            <Box width='175px' paddingLeft='24px'>Amount</Box>
+                            <Box width='96px'></Box>
+
+                        </Box>
+                        {equipments.map((equipment) => {
+                            return (<Box className={"summaryBar"} background={"white"} height='40px' display={"flex"}
+                                         flexDirection={"row"} marginTop='12px' marginBottom='12px'
+                                         key={equipment.itemId}>
+                                <Box width='210px' marginLeft='16px' paddingLeft='24px'>{equipment.equipmentType}</Box>
+                                <Box width='252px' paddingLeft='24px'>{equipment.brand}</Box>
+                                <Box width='130px' paddingLeft='24px'>{equipment.totalPrice}</Box>
+                                <Box width='175px' paddingLeft='24px'>{equipment.amount}</Box>
+                                <Box width='96px' display={"flex"} justifyContent={"center"}>
+                                    <img alt={"Bin"} src={Bin} onClick={() => deleteItem(equipment.itemId)}/>
+                                </Box>
+                            </Box>)
+                        })}
+                    </Box>
+                    <Box width='908px' display={"flex"} flexDirection={"row"} justifyContent={"space-between"}
+                         marginTop='20px'>
+                        <Box onClick={() => navigate('/resort')} display={"flex"} flexDirection={"row"} width='200px'>
+                            <Box className={"backToShop"} marginLeft='14px' width='144px' height='40px'> Back to
+                                Shop </Box></Box>
+                        <button className={"buttonAdd"} onClick={() => confirmBill()}>Confirm
+                            Order
+                        </button>
 
                     </Box>
-                    {equipments.map((equipment) => {
-                        return (<Box className={"summaryBar"} background={"white"} height='40px' display={"flex"}
-                                     flexDirection={"row"} marginTop='12px' marginBottom='12px' key={equipment.itemId}>
-                            <Box width='210px' marginLeft='16px' paddingLeft='24px'>{equipment.equipmentType}</Box>
-                            <Box width='252px' paddingLeft='24px'>{equipment.brand}</Box>
-                            <Box width='130px' paddingLeft='24px'>{equipment.totalPrice}</Box>
-                            <Box width='175px' paddingLeft='24px'>{equipment.amount}</Box>
-                            <Box width='96px' display={"flex"} justifyContent={"center"}>
-                                <img alt={"Bin"} src={Bin} onClick={() => deleteItem(equipment.itemId)}/>
-                            </Box>
-                        </Box>)
-                    })}
                 </Box>
-                <Box width='908px' display={"flex"} flexDirection={"row"} justifyContent={"space-between"}
-                     marginTop='20px'>
-                    <Box onClick={() => navigate('/resort')} display={"flex"} flexDirection={"row"} width='200px'>
-                        <Box className={"backToShop"} marginLeft='14px' width='144px' height='40px'> Back to Shop </Box></Box>
-                    <button className={"buttonAdd"} onClick={() => [confirmBill(), navigate("/confirmation")]}>Confirm
-                        Order
-                    </button>
-                </Box>
-                <Box>{bill.id}</Box>
-                <Box>{context.billData.id}</Box>
-            </Box>
-
-        </Box>)
-}
+            </Box>)
+    }
